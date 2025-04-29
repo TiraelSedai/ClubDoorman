aadbdf09b1f43018d21c349789b55d20eb339c8c
eXpressionist <expressioned@gmail.com>
2025-04-29 23:30:06 +0300

    Update Worker.cs

--- ClubDoorman/Worker.cs
+++ ClubDoorman/Worker.cs
@@ -262,14 +262,7 @@ internal sealed class Worker(
         if (message.ReplyMarkup != null)
         {
             _logger.LogDebug("Buttons");
-            if (Config.ButtonAutoBan)
-            {
-                await DeleteAndReportMessage(message, "Сообщение с кнопками", stoppingToken);
-            }
-            else
-            {
-                await AutoBan(message, "Сообщение с кнопками", stoppingToken);
-            }
+			await (Config.ButtonAutoBan ? AutoBan(message, "Сообщение с кнопками", stoppingToken) : DeleteAndReportMessage(message, "Сообщение с кнопками", stoppingToken));
             return;
         }
         if (message.Story != null)
