690df330cc3b2a56292b072f5a5dc3a0de2b307b
Evgenii Burmistrov <me@burm.dev>
2026-05-04 10:18:58 +0300

    add 'hola'

--- ClubDoorman/SimpleFilters.cs
+++ ClubDoorman/SimpleFilters.cs
@@ -5,7 +5,7 @@ namespace ClubDoorman;
 public static class SimpleFilters
 {
     private static readonly string[] StopWords = File.ReadAllLines("data/stop-words.txt");
-    private static readonly string[] HelloWordsStop = ["привет", "hi", "hello", "yo", "الو", "سلام"];
+    private static readonly string[] HelloWordsStop = ["привет", "hi", "hello", "hola", "yo", "الو", "سلام"];
 
     public static bool HasOnlyHelloWord(string message) => HelloWordsStop.Any(sw => message.Equals(sw, StringComparison.OrdinalIgnoreCase));
 
